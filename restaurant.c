327432e6ecea64cbb977bf18c5d03c61325eeea4
Maharshi Basu <basumaharshi10@gmail.com>
2022-06-20 19:47:44 +0530

    add psedo code for usesr input

--- restaurant.c
+++ restaurant.c
@@ -2,7 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-skip_line(FILE *fp)
+#define MAX 100
+
+typedef struct node {
+    int s_no;
+    char name[50];
+    int price;
+    int quant_sold;
+    struct node *next;
+} node;
+
+node *head = NULL;
+int size = 0;
+
+// void add_item(char *name , char type , int price , int quantity_sold) {
+
+//     if (size >= MAX) {
+//         fprintf(stderr , "STACK OVERFLOW!\n");
+//         exit(0);
+//     }
+
+//     node *new_item = (node *)malloc(sizeof(node));
+//     strncpy(name , new_item->name , sizeof(name));
+//     //new_item->type = type;
+//     new_item->price = price;
+//     new_item->quant_sold = quantity_sold;
+//     new_item->next = HEAD;
+//     HEAD = new_item;
+
+//     size++; 
+//     printf("NEW ITEM ADDED TO MENU!\n");
+// }
+
+int skip_line(FILE *fp)
 {
     int c;
 
@@ -12,8 +44,24 @@ skip_line(FILE *fp)
     return c;
 }
 
+void display_menu() {
+    node *temp;
+    if (head == NULL) {
+        printf("MENU IS EMPTY.\n");
+        return;
+    } 
+    temp = head;
+    printf("S.NO\t\tNAME\t\tPRICE\t\tQUANTITY\n\n");
+    while (temp != NULL) {
+        printf("%d\t\t%s\t\t%d\t\t%d\n" , temp->s_no , temp->name , temp->price , temp->quant_sold);
+        temp = temp->next;
+    }
+}
+
 int main()
 {
+    node *newNode , *temp;
+
     FILE *the_file = fopen("excelinput.csv", "r");
     if (the_file == NULL)
     {
@@ -21,6 +69,35 @@ int main()
         exit(1);
     }
     char line[200];
+    skip_line(the_file);
+    int i = 1;
+    head = (node *)malloc(sizeof(node));
+    int first_line = 0;
+    while (fgets(line , sizeof(line) , the_file)) {
+        char *val;
+        val = strtok(line , ",");
+        if (first_line == 4) {
+            break;
+        }
+        else if (first_line == 0) {
+            head->s_no = atoi(val);
+            first_line++;
+        }
+        else if (first_line == 1) {
+            strncpy (val , head->name , sizeof(val));
+            first_line++;
+        }
+        else if (first_line == 2) {
+            head->price = atoi(val);
+            first_line++;
+        }
+        else if (first_line == 3) {
+            head->quant_sold = atoi(val);
+        }
+    }
+    head->next = NULL;
+    temp = head;
+    skip_line(the_file);
     skip_line(the_file);
 
     while (fgets(line, sizeof(line), the_file))
@@ -33,11 +110,35 @@ int main()
 
         while (token != NULL)
         {
-            printf("%-12s", token);
-            // The first call to strtok must pass the C string to tokenize, and subsequent calls must specify NULL as the first argument, which tells the function to continue tokenizing the string you passed in first.
 
+            //printf("%-12s", token);
+            // The first call to strtok must pass the C string to tokenize, and subsequent calls must specify NULL as the first argument, which tells the function to continue tokenizing the string you passed in first.
             token = strtok(NULL, ",");
+            newNode = (node *)malloc(sizeof(node));
+            if (i == 1) {
+                newNode->s_no = atoi(token);
+                i++;
+            }
+            else if (i == 2) {
+                strncpy(token , newNode->name , sizeof(token));
+                i++;
+            }
+            else if (i == 3) {
+                newNode->price = atoi(token);
+                i++;
+            }
+            else if (i == 4) {
+                newNode->quant_sold = atoi(token);
+                i++;
+            }
         }
-        printf("\n");
+        newNode->next = NULL;
+        temp->next = newNode;
+        temp = temp->next;
+        i = 1;
+        //printf("\n");
     }
+
+    display_menu();
+    return 0;
 }
