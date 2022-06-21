385ad8c1b50c46e47d399f6cee100416f60f779e
k3yss <rsi.sxs@gmail.com>
2022-06-22 00:23:16 +0530

    add kitchen menu

--- restaurant.c
+++ restaurant.c
@@ -12,86 +12,6 @@ struct node
 
 struct node *head;
 
-void MergeSort()
-{
-    struct Node **headRef = &head;
-    struct Node *temp = *headRef;
-    struct Node *a;
-    struct Node *b;
-
-    /* Base case -- length 0 or 1 */
-    if ((head == NULL) || (head->next == NULL))
-    {
-        return;
-    }
-
-    /* Split head into 'a' and 'b' sublists */
-    FrontBackSplit(head, &a, &b);
-
-    /* Recursively sort the sublists */
-    MergeSort(&a);
-    MergeSort(&b);
-
-    /* answer = merge the two sorted lists together */
-    *headRef = SortedMerge(a, b);
-}
-
-// OKABE LINK
-// https://www.geeksforgeeks.org/merge-sort-for-linked-list/
-struct Node *SortedMerge(struct Node *a, struct Node *b)
-{
-    struct Node *result = NULL;
-
-    /* Base cases */
-    if (a == NULL)
-        return (b);
-    else if (b == NULL)
-        return (a);
-
-    /* Pick either a or b, and recur */
-    if (a->data <= b->data)
-    {
-        result = a;
-        result->next = SortedMerge(a->next, b);
-    }
-    else
-    {
-        result = b;
-        result->next = SortedMerge(a, b->next);
-    }
-    return (result);
-}
-
-/* UTILITY FUNCTIONS */
-/* Split the nodes of the given list into front and back halves,
-    and return the two lists using the reference parameters.
-    If the length is odd, the extra node should go in the front list.
-    Uses the fast/slow pointer strategy. */
-void FrontBackSplit(struct Node *source,
-                    struct Node **frontRef, struct Node **backRef)
-{
-    struct Node *fast;
-    struct Node *slow;
-    slow = source;
-    fast = source->next;
-
-    /* Advance 'fast' two nodes, and advance 'slow' one node */
-    while (fast != NULL)
-    {
-        fast = fast->next;
-        if (fast != NULL)
-        {
-            slow = slow->next;
-            fast = fast->next;
-        }
-    }
-
-    /* 'slow' is before the midpoint in the list, so split it in two
-    at that point. */
-    *frontRef = source;
-    *backRef = slow->next;
-    slow->next = NULL;
-}
 int skip_line(FILE *fp)
 {
     int c;
@@ -162,8 +82,95 @@ void push(char *line)
         ptr->next = head;
         head = ptr;
     }
-    printf("Item pushed");
+    // printf("Item pushed");
 }
+
+/* function prototypes */
+struct node *SortedMerge(struct node *a, struct node *b);
+void FrontBackSplit(struct node *source,
+                    struct node **frontRef, struct node **backRef);
+
+/* sorts the linked list by changing next pointers (not data) */
+void MergeSort(struct node **headRef)
+{
+    struct node *head1 = *headRef;
+    struct node *a;
+    struct node *b;
+
+    /* Base case -- length 0 or 1 */
+    if ((head1 == NULL) || (head1->next == NULL))
+    {
+        return;
+    }
+
+    /* Split head into 'a' and 'b' sublists */
+    FrontBackSplit(head1, &a, &b);
+
+    /* Recursively sort the sublists */
+    MergeSort(&a);
+    MergeSort(&b);
+
+    /* answer = merge the two sorted lists together */
+    *headRef = SortedMerge(a, b);
+}
+
+/* See https:// www.geeksforgeeks.org/?p=3622 for details of this
+function */
+struct node *SortedMerge(struct node *a, struct node *b)
+{
+    struct node *result = NULL;
+
+    /* Base cases */
+    if (a == NULL)
+        return (b);
+    else if (b == NULL)
+        return (a);
+
+    /* Pick either a or b, and recur */
+    if (a->quan >= b->quan)
+    {
+        result = a;
+        result->next = SortedMerge(a->next, b);
+    }
+    else
+    {
+        result = b;
+        result->next = SortedMerge(a, b->next);
+    }
+    return (result);
+}
+
+/* UTILITY FUNCTIONS */
+/* Split the nodes of the given list into front and back halves,
+    and return the two lists using the reference parameters.
+    If the length is odd, the extra node should go in the front list.
+    Uses the fast/slow pointer strategy. */
+void FrontBackSplit(struct node *source,
+                    struct node **frontRef, struct node **backRef)
+{
+    struct node *fast;
+    struct node *slow;
+    slow = source;
+    fast = source->next;
+
+    /* Advance 'fast' two nodes, and advance 'slow' one node */
+    while (fast != NULL)
+    {
+        fast = fast->next;
+        if (fast != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+    }
+
+    /* 'slow' is before the midpoint in the list, so split it in two
+    at that point. */
+    *frontRef = source;
+    *backRef = slow->next;
+    slow->next = NULL;
+}
+
 void display()
 {
 
@@ -175,14 +182,15 @@ void display()
     }
     else
     {
-        printf("Printing Stack elements \n");
+        printf("-----------------TODAYS MENU TO BE PREPARED------------------------- \n");
         while (ptr != NULL)
         {
-            printf("%d %s\n", ptr->srNo, ptr->proName);
+            printf("%d %s %d %d %s \n", ptr->srNo, ptr->proName, ptr->price, ptr->quan, ptr->preference);
             ptr = ptr->next;
         }
     }
 }
+
 int main()
 {
     FILE *the_file = fopen("excelinput.csv", "r");
@@ -198,7 +206,9 @@ int main()
     {
         push(line);
         // disk(line);
-        printf("\n");
+        // printf("\n");
     }
+
+    MergeSort(&head);
     display();
 }
