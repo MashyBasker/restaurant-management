03b4ec8b555b3f9143304a0cae87e4a3f08db6bd
k3yss <rsi.sxs@gmail.com>
2022-06-21 23:35:28 +0530

    implement sort

--- restaurant.c
+++ restaurant.c
@@ -12,6 +12,86 @@ struct node
 
 struct node *head;
 
+void MergeSort()
+{
+    struct Node **headRef = &head;
+    struct Node *temp = *headRef;
+    struct Node *a;
+    struct Node *b;
+
+    /* Base case -- length 0 or 1 */
+    if ((head == NULL) || (head->next == NULL))
+    {
+        return;
+    }
+
+    /* Split head into 'a' and 'b' sublists */
+    FrontBackSplit(head, &a, &b);
+
+    /* Recursively sort the sublists */
+    MergeSort(&a);
+    MergeSort(&b);
+
+    /* answer = merge the two sorted lists together */
+    *headRef = SortedMerge(a, b);
+}
+
+// OKABE LINK
+// https://www.geeksforgeeks.org/merge-sort-for-linked-list/
+struct Node *SortedMerge(struct Node *a, struct Node *b)
+{
+    struct Node *result = NULL;
+
+    /* Base cases */
+    if (a == NULL)
+        return (b);
+    else if (b == NULL)
+        return (a);
+
+    /* Pick either a or b, and recur */
+    if (a->data <= b->data)
+    {
+        result = a;
+        result->next = SortedMerge(a->next, b);
+    }
+    else
+    {
+        result = b;
+        result->next = SortedMerge(a, b->next);
+    }
+    return (result);
+}
+
+/* UTILITY FUNCTIONS */
+/* Split the nodes of the given list into front and back halves,
+    and return the two lists using the reference parameters.
+    If the length is odd, the extra node should go in the front list.
+    Uses the fast/slow pointer strategy. */
+void FrontBackSplit(struct Node *source,
+                    struct Node **frontRef, struct Node **backRef)
+{
+    struct Node *fast;
+    struct Node *slow;
+    slow = source;
+    fast = source->next;
+
+    /* Advance 'fast' two nodes, and advance 'slow' one node */
+    while (fast != NULL)
+    {
+        fast = fast->next;
+        if (fast != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+    }
+
+    /* 'slow' is before the midpoint in the list, so split it in two
+    at that point. */
+    *frontRef = source;
+    *backRef = slow->next;
+    slow->next = NULL;
+}
 int skip_line(FILE *fp)
 {
     int c;
@@ -22,10 +102,6 @@ int skip_line(FILE *fp)
     return c;
 }
 
-void disk(char *line)
-{
-    printf("%s", line);
-}
 void push(char *line)
 {
 
