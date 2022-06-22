11ac4e4226d982cc1d2491b57e72fb349a87d464
k3yss <rsi.sxs@gmail.com>
2022-06-22 12:15:50 +0530

    update csv

--- app.c
+++ app.c
@@ -1,50 +1,67 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sack.h"
-#include <string.h>
 
 #define MAX 100
 
 struct node *head, *veg_head, *nveg_head;
-// int size1 = 0;
 int veg_siz = 0, nveg_siz = 0;
-
-void push_veg(int s_no , char name[50], int price, int quant, char type)
+void pushing(int s_no, char name[50], int price, int quant, char type)
+{
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->quan = quant;
+    new_node->srNo = s_no;
+    strcpy(new_node->proName, name);
+    new_node->price = price;
+    new_node->preference = type;
+    if (type == "V")
+    {
+        new_node->next = veg_head;
+        veg_head = new_node;
+    }
+    else
+    {
+        new_node->next = nveg_head;
+        nveg_head = new_node;
+    }
+}
+/*
+void push_veg(int s_no, char name[50], int price, int quant, char type)
 {
     if (veg_siz >= MAX)
     {
         printf("STACK OVERFLOW.\n");
         exit(0);
     }
-    struct node *new_node = (struct node *)malloc(sizeof(struct node));
-    new_node->quan = quant;
-    new_node->srNo = s_no;
-    strcpy(new_node->proName, name);
-    new_node->price = price;
-    new_node->preference = type;
-    new_node->next = veg_head;
-    veg_head = new_node;
+    pushing(s_no, name, price, quant, type);
+
     veg_siz++;
 }
 
-void push_nveg(int s_no , char name[50], int price, int quant, char type)
+void push_nveg(int s_no, char name[50], int price, int quant, char type)
 {
     if (nveg_siz >= MAX)
     {
         printf("STACK OVERFLOW.\n");
         exit(0);
     }
-    struct node *new_node = (struct node *)malloc(sizeof(struct node));
-    new_node->quan = quant;
-    new_node->srNo = s_no;
-    strcpy(new_node->proName, name);
-    new_node->price = price;
-    new_node->preference = type;
-    new_node->next = nveg_head;
-    nveg_head = new_node;
     nveg_siz++;
+}*/
+void create_list()
+{
+    struct node *temp = budget_head;
+    while (temp != NULL)
+    {
+        if (temp->preference == 'V')
+        {
+            push_veg(temp->srNo, temp->proName, temp->price, temp->quan, temp->preference);
+        }
+        else if (temp->preference == 'N')
+        {
+            push_nveg(temp->srNo, temp->proName, temp->price, temp->quan, temp->preference);
+        }
+        temp = temp->next;
+    }
 }
-
+/*
 void create_veg_list()
 {
     struct node *temp = budget_head;
@@ -52,9 +69,10 @@ void create_veg_list()
     {
         if (temp->preference == 'V')
         {
-            push_veg(temp->srNo , temp->proName, temp->price, temp->quan, temp->preference);
+            push_veg(temp->srNo, temp->proName, temp->price, temp->quan, temp->preference);
         }
-        temp = temp->next;
+        else
+            temp = temp->next;
     }
 }
 
@@ -65,38 +83,42 @@ void create_nveg_list()
     {
         if (temp->preference == 'N')
         {
-            push_nveg(temp->srNo , temp->proName, temp->price, temp->quan, temp->preference);
+            push_nveg(temp->srNo, temp->proName, temp->price, temp->quan, temp->preference);
         }
         temp = temp->next;
     }
 }
-
-void generate_bill(struct node *temp_head) {
+*/
+void generate_bill(struct node *temp_head)
+{
     int order_no;
     printf("HOW MANY ITEMS DO YOU WANT TO ORDER?: ");
-    scanf("%d" , &order_no);
+    scanf("%d", &order_no);
     printf("ENTER THE SERIAL NOS. OF THE ITEMS:\n ");
     int order_list[order_no];
-    for (int i = 0 ; i < order_no ; i++) {
-        printf("SERIAL NO. OF ORDER %d: " , i + 1);
-        scanf("%d" , &order_list[i]);
+    for (int i = 0; i < order_no; i++)
+    {
+        printf("SERIAL NO. OF ORDER %d: ", i + 1);
+        scanf("%d", &order_list[i]);
     }
     int sum = 0;
     struct node *p = temp_head;
     printf("YOUR BILL\n");
-    for (int i = 0 ; i < order_no ; i++) {
+    for (int i = 0; i < order_no; i++)
+    {
         int serial_no = order_list[i];
-        while (p != NULL) {
-            if (p->srNo == serial_no) {
-                printf("ORDER %d: %s\n" , i + 1 , p->proName);
+        while (p != NULL)
+        {
+            if (p->srNo == serial_no)
+            {
+                printf("ORDER %d: %s\n", i + 1, p->proName);
                 sum += p->price;
             }
             p = p->next;
         }
         p = temp_head;
     }
-    printf("TOTAL: %d\n" , sum);
-
+    printf("TOTAL: %d\n", sum);
 }
 
 int main()
@@ -135,54 +157,35 @@ int main()
     // create_menu_list();
     char choice;
     printf("DO YOU WANT VEG OR NON-VEG FOOD?[V/N]: ");
-    scanf(" %c" , &choice);
-    if (choice == 'V') {
-        create_veg_list();
+    scanf(" %c", &choice);
+    if (choice == 'V')
+    {
+        // create_veg_list();
+        create_list();
         struct node *ptr1 = veg_head;
         printf("S.NO\t\tNAME\t\tPRICE\t\tQUANTITY SOLD\t\tMENU TYPE\n");
         printf("+--------------------------------------------------------------------------------+\n\n");
-        while (ptr1 != NULL) {
-            printf("%d\t\t%s\t\t%d\t\t%d\t\t%c\n" , ptr1->srNo , ptr1->proName , ptr1->price , ptr1->quan , ptr1->preference);
+        while (ptr1 != NULL)
+        {
+            printf("%d\t\t%s\t\t%d\t\t%d\t\t%c\n", ptr1->srNo, ptr1->proName, ptr1->price, ptr1->quan, ptr1->preference);
             ptr1 = ptr1->next;
         }
         generate_bill(veg_head);
     }
-    else if (choice == 'N') {
-        create_nveg_list();
+    else if (choice == 'N')
+    {
+        // create_nveg_list();
+        create_list();
         struct node *q = nveg_head;
         printf("S.NO\t\tNAME\t\tPRICE\t\tQUANTITY SOLD\t\tMENU TYPE\n");
         printf("+--------------------------------------------------\n\n");
-        while (q != NULL) {
-            printf("%d\t\t%s\t\t%d\t\t%d\t\t%c\n" , q->srNo , q->proName , q->price , q->quan , q->preference);
+        while (q != NULL)
+        {
+            printf("%d\t\t%s\t\t%d\t\t%d\t\t%c\n", q->srNo, q->proName, q->price, q->quan, q->preference);
             q = q->next;
         }
         generate_bill(nveg_head);
     }
 
-    //create_veg_list();
-    //create_nveg_list();
-    /*
-    printf("########### THE VEG MENU ###########\n\n");
-    struct node *ptr1 = veg_head;
-    int i = 1;
-    while (ptr1 != NULL)
-    {
-        printf("%d\t\t%s\t\t%d\t\t%d\t\t%c\n", i, ptr1->proName, ptr1->price, ptr1->quan, ptr1->preference);
-        i++;
-        ptr1 = ptr1->next;
-    }
-    */
-    //printf("\n");
-    /*
-    int j = 1;
-    struct node *qt = nveg_head;
-    printf("######### THE NON-VEG MENU ##########\n\n");
-    while (qt != NULL)
-    {
-        printf("%d\t\t%s\t\t%d\t\t%d\t\t%c\n", j, qt->proName, qt->price, qt->quan, qt->preference);
-        j++;
-        qt = qt->next;
-    }
-    */
     return 0;
 }
--- sack.h
+++ sack.h
@@ -1,60 +1,11 @@
-#include <stdio.h>
 #include "restaurant.h"
-#include <stdlib.h>
-#include <string.h>
 
 #define MAX 100
-
-/*
-typedef struct node
-{
-    char prod_name[50], prod_type;
-    int prod_price, prod_quant_sold;
-    struct node *next;
-} node;
-*/
 
 struct node *budget_head = NULL;
 int size = 0, bug_size = 0;
 
-/*
-void create_menu_list()
-{
-    int no_items, item_price, items_sold;
-    char item_name[50], item_type;
-    printf("HOW MANY ITEMS ARE PRESENT IN THE MENU: ");
-    scanf("%d", &no_items);
-    for (int i = 0; i < no_items; i++)
-    {
-        printf("ENTER THE NAME OF THE ITEM %d: ", i + 1);
-        scanf("%s", &item_name);
-        printf("ENTER THE PRICE OF THE ITEM %d: ", i + 1);
-        scanf("%d", &item_price);
-        printf("ENTER THE TYPE OF THE ITEM %d [V/N]: ", i + 1);
-        scanf(" %c", &item_type);
-        printf("ENTER THE NO. OF ITEM %d SOLD IN PAST WEEK: ", i + 1);
-        scanf("%d", &items_sold);
-        push_back(item_name, item_price, items_sold, item_type);
-    }
-}
-void push_back(char name[50], int price, int quant, char type)
-{
-    if (size >= MAX)
-    {
-        printf("STACK OVERFLOW.\n");
-        exit(0);
-    }
-    struct node *new_node = (node *)malloc(sizeof(node));
-    strcpy(new_node->prod_name, name);
-    new_node->prod_quant_sold = quant;
-    new_node->prod_price = price;
-    new_node->prod_type = type;
-    new_node->next = head;
-    head = new_node;
-    size++;
-}
-*/
-void push_budget(int s_no , char name[50], int price, int quant, char type)
+void push_budget(int s_no, char name[50], int price, int quant, char type)
 {
     if (bug_size >= MAX)
     {
@@ -79,7 +30,7 @@ void budget_list(int budget_amt)
     {
         if (temp->price <= budget_amt)
         {
-            push_budget(temp->srNo , temp->proName, temp->price, temp->quan, temp->preference);
+            push_budget(temp->srNo, temp->proName, temp->price, temp->quan, temp->preference);
         }
         temp = temp->next;
     }
